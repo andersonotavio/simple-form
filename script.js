5a0624747cab50d5afe746113797bd485303744d
andersonotavio <andersonotavio@hotmail.com>
2021-04-05 15:33:11 -0300

    fistCommit

---
+++ script.js
@@ -0,0 +1,94 @@
+let formValidator = {
+    handleSubmit: (event)=>{
+        event.preventDefault();
+        let send = true;
+        let inputs = document.querySelectorAll('input')
+
+        formValidator.clearErros()
+
+        for(let i = 0; i < inputs.length; i++){
+            let input = inputs[i];
+            let check = formValidator.checkInput(input);
+            if( check !== true){
+                send = false;
+                formValidator.showError(input, check)
+            }
+        }
+        
+        
+        if(send){
+            form.submit();
+        }
+    },
+    checkInput: (input) => {
+        let rules = input.getAttribute('data-rules');
+        
+        if(rules !== null){
+           rules = rules.split('|')
+           for(let k in rules){
+               let rDetails = rules[k].split('=');
+               switch (rDetails[0]) {
+                   case 'required':
+                       if(input.value == ''){
+                           return 'Campo não pode ser vazio.';
+                       }
+                       
+                       break;
+               
+                   case 'min':
+                       if(input.value.length < rDetails[1]){
+                           return 'Campo tem que ter pelo menos '+rDetails[1] + ' caracters'
+                       }
+                       break;
+                    case 'email':
+                        if(input.value != ''){
+                            let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                            if(!regex.test(input.value.toLowerCase())){
+                                return 'E-mail não é valido'
+                            }
+                        }
+                        break;
+               }
+
+           }
+            
+        }
+       return true;
+    },
+    showError: (input, error) =>{
+        input.style.borderColor = 'red';
+
+        let errorElement = document.createElement('div')
+        errorElement.classList.add('error')
+        errorElement.innerHTML = error
+
+        input.parentElement.insertBefore(errorElement, input.elementSibling)
+    },
+    clearErros: () => {
+        let inputs = document.querySelectorAll('input')
+        for(let i = 0; i < inputs.length; i++){
+            inputs[i].style = ''
+        }
+
+
+        let errorElement = document.querySelectorAll('.error')
+        for(let i = 0; i < errorElement.length; i++){
+            errorElement[i].remove()
+        }
+    }
+}
+
+function cor(){
+    let botao = document.querySelector('.botao')
+    botao.style.background = 'blue'
+    botao.style.color = 'white'
+    botao.style.transition = '0.5s'
+}
+function corA(){
+    let botao = document.querySelector('.botao')
+    botao.style.background = ''
+    botao.style.color = ''
+}
+
+let form = document.querySelector('.validator')
+form.addEventListener('submit', formValidator.handleSubmit);
